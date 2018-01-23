1b41d46dbe565196fa2ffe8720ed12d33e66d195
jfernandogg <juan.gallego@gmail.com>
2018-01-23 15:58:00 -0500

    added thumbnail support
    added thumbnail support in posts
    also changed post index theme

--- tarottheme/functions.php
+++ tarottheme/functions.php
@@ -1,8 +1,20 @@
 <?php
 require_once get_template_directory() . '/wp-bootstrap-navwalker.php';
-register_nav_menus( array(
-    'primary' => __( 'Primary Menu', 'tarottheme' ),
-) );
 
+function adv_theme_support() {
 
+	add_theme_support('post_thumbnail');
+	register_nav_menus( array(
+	    'primary' => __( 'Primary Menu', 'tarottheme' ),
+	) );
+
+}
+
+add_action('after_setup_theme','adv_theme_support');
+
+function adv_set_excerpt_length() {
+	return 25;
+}
+
+add_filter('excerpt_length','adv_set_excerpt_length');
 ?>
--- tarottheme/index.php
+++ tarottheme/index.php
@@ -2,34 +2,36 @@
 
 <?php if(have_posts()): ?>
   <?php while(have_posts()) : the_post(); ?>
-    <section class="engine"><a href="https://mobirise.co/m">bootstrap table</a></section><section class="mbr-section mbr-after-navbar" id="header3-r" data-rv-view="11">
-        <div class="mbr-section__container container mbr-section__container--first">
-            <div class="mbr-header mbr-header--wysiwyg row">
-                <div class="col-sm-8 col-sm-offset-2">
-                    <h3 class="mbr-header__text"><?php echo the_title(); ?></h3>
-                    <p class="mbr-header__subtext">By <?php the_author(); ?> posted <?php the_date(); ?></p>
+    <section class="engine"><a href="https://mobirise.co/d">free web software</a></section><section class="mbr-section mbr-section--relative mbr-after-navbar" id="msg-box4-v" data-rv-view="23" style="background-color: rgb(71, 85, 119);">
+        
+        <div class="mbr-section__container mbr-section__container--isolated container" style="padding-top: 93px; padding-bottom: 93px;">
+            <div class="row">
+                <div class="mbr-box mbr-box--fixed mbr-box--adapted">
+                    <div class="mbr-box__magnet mbr-box__magnet--top-right mbr-section__left col-sm-6 image-size" style="width: 50%;">
+                        <figure class="mbr-figure mbr-figure--adapted mbr-figure--caption-inside-bottom mbr-figure--full-width"><?php the_post_thubnails('thumbnail',['class'=>'mbr-figure__img']); ?></figure>
+                    </div>
+                    <div class="mbr-box__magnet mbr-class-mbr-box__magnet--center-left col-sm-6 content-size mbr-section__right">
+                        <div class="mbr-section__container mbr-section__container--middle">
+                            <div class="mbr-header mbr-header--auto-align mbr-header--wysiwyg">
+                                <h3 class="mbr-header__text"><?php echo the_title(); ?></h3>
+                                <p class="mbr-header__subtext">By <?php the_author(); ?> posted on<?php the_date(); ?></p>
+                                
+                            </div>
+                        </div>
+                        <div class="mbr-section__container mbr-section__container--middle">
+                            <div class="mbr-article mbr-article--auto-align mbr-article--wysiwyg"><?php echo get_the_excerpt(); ?></div>
+                        </div>
+                        <div class="mbr-section__container">
+                            <div class="mbr-buttons mbr-buttons--auto-align btn-inverse"><a class="mbr-buttons__btn btn btn-lg btn-default" href="<?php the_permalink(); ?>">LEARN MORE</a></div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
     </section>
 
-    <section class="mbr-section" id="content1-s" data-rv-view="13">
-        <div class="mbr-section__container container mbr-section__container--middle">
-            <div class="row">
-                <div class="mbr-article mbr-article--wysiwyg col-sm-8 col-sm-offset-2"><?php echo get_the_excerpt(); ?></div>
-            </div>
-        </div>
-    </section>
 
-    <section class="mbr-section" id="buttons1-t" data-rv-view="14">
-        <div class="mbr-section__container container mbr-section__container--last">
-            <div class="row">
-                <div class="col-sm-8 col-sm-offset-2">
-                    <div class="mbr-buttons mbr-buttons--center"><a class="mbr-buttons__btn btn btn-lg btn-danger" href="<?php the_permalink(); ?>">Leer Mas</a> </div>
-                </div>
-            </div>
-        </div>
-    </section>
 
 
   <?php endwhile; ?>
